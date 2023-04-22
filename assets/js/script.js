ec74f37d4e3bdc3e59e4ce109d4aa0094548da06
AdrienClinet <130060672+AdrienClinet@users.noreply.github.com>
2023-04-22 16:48:18 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,25 +135,27 @@ for (let i = 0; i < formInputs.length; i++) {
 }
 
 
-
 // page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const navigationLinks = document.querySelectorAll(".navbar-link");
 const pages = document.querySelectorAll("[data-page]");
+
+// function to change the active page
+const changePage = function (targetPage) {
+  for (let i = 0; i < pages.length; i++) {
+    if (pages[i].dataset.page === targetPage) {
+      pages[i].classList.add("active");
+      navigationLinks[i].classList.add("active");
+    } else {
+      pages[i].classList.remove("active");
+      navigationLinks[i].classList.remove("active");
+    }
+  }
+}
 
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-    const targetPage = this.innerHTML.toLowerCase();
-
-    for (let j = 0; j < pages.length; j++) {
-      if (targetPage === pages[j].dataset.page.toLowerCase()) {
-        pages[j].classList.add("active");
-        navigationLinks[j].classList.add("active");
-      } else {
-        pages[j].classList.remove("active");
-        navigationLinks[j].classList.remove("active");
-      }
-    }
+    changePage(this.dataset.navLink);
 
     // close sidebar on mobile when clicking on a nav link
     if (window.innerWidth <= 768) {
