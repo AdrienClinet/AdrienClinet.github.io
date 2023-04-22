4cb3fb0d6b7f77505ad44c19710a09d4d6db66e6
AdrienClinet <130060672+AdrienClinet@users.noreply.github.com>
2023-04-22 16:29:42 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,31 +135,24 @@ for (let i = 0; i < formInputs.length; i++) {
 }
 
 
+
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// function to change the active page
-const changePage = function (link, index) {
-  for (let i = 0; i < pages.length; i++) {
-    if (i === index) {
-      pages[i].classList.add("active");
-      navigationLinks[i].classList.add("active");
-    } else {
-      pages[i].classList.remove("active");
-      navigationLinks[i].classList.remove("active");
-    }
-  }
-}
-
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-    changePage(this, i);
+    const targetPage = this.innerHTML.toLowerCase();
 
-    // close sidebar on mobile when clicking on a nav link
-    if (window.innerWidth <= 768) {
-      elementToggleFunc(sidebar);
+    for (let j = 0; j < pages.length; j++) {
+      if (targetPage === pages[j].dataset.page.toLowerCase()) {
+        pages[j].classList.add("active");
+        navigationLinks[j].classList.add("active");
+      } else {
+        pages[j].classList.remove("active");
+        navigationLinks[j].classList.remove("active");
+      }
     }
   });
 }
